e6c21400544198e5a2f96b84d98ad37e931d4210
Michael Spaxman <seanforyou23@gmail.com>
2018-11-28 11:12:47 -0500

    feat(a11y-checks): run accessibility checks on generated markup during dev

--- src/index.js
+++ src/index.js
@@ -2,4 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+if (process.env.NODE_ENV !== 'production') {
+  var axe = require('react-axe');
+  axe(React, ReactDOM, 1000);
+}
+
 ReactDOM.render(<App />, document.getElementById("root"));
