06293f43cb6b3f1f994d2d96735ec1f0d9eec9b5
Michael Spaxman <seanforyou23@gmail.com>
2019-01-21 10:15:59 -0500

    feat(webpack): add support for image, font, and fonticon assets

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 import "@patternfly/react-core/dist/styles/base.css";
+import App from "./App";
 
 if (process.env.NODE_ENV !== "production") {
   var axe = require("react-axe");
