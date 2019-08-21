b6a4d8d6b20f7db4e31b581e74bfbd8fe4ea3b42
Michael Spaxman <seanforyou23@gmail.com>
2019-08-20 22:15:30 -0400

    chore(deps): update dependencies and configs to match

--- webpack.common.js
+++ webpack.common.js
@@ -94,9 +94,12 @@ module.exports = {
           path.resolve(__dirname, 'src'),
           path.resolve(__dirname, 'node_modules/patternfly'),
           path.resolve(__dirname, 'node_modules/@patternfly/patternfly/assets'),
-          path.resolve(__dirname, 'node_modules/@patternfly/react-core/dist/styles/assets/images'),
+          path.resolve(__dirname, 'node_modules/@patternfly/patternfly/assets/images'),
           path.resolve(__dirname, 'node_modules/@patternfly/react-styles/css/assets/images'),
-          path.resolve(__dirname, 'node_modules/@patternfly/react-core/node_modules/@patternfly/react-styles/css/assets/images')
+          path.resolve(__dirname, 'node_modules/@patternfly/react-core/dist/styles/assets/images'),
+          path.resolve(__dirname, 'node_modules/@patternfly/react-core/node_modules/@patternfly/react-styles'),
+          path.resolve(__dirname, 'node_modules/@patternfly/react-core/node_modules/@patternfly/react-styles/css/assets/images'),
+          path.resolve(__dirname, 'node_modules/@patternfly/react-table/node_modules/@patternfly/react-styles/css/assets/images'),
         ],
         use: [
           {
