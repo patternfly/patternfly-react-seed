6e06ebd337803b1c5ef440105381ea2aa5d89150
Michael Spaxman <seanforyou23@gmail.com>
2020-02-26 09:35:57 -0500

    [cleanup] format code, remove react-axe

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { App } from '@app/index';
 
-// if (process.env.NODE_ENV !== "production") {
-//   const axe = require("react-axe"); // eslint-disable-line
-//   axe(React, ReactDOM, 1000);
-// }
-
 ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
