0e2f47f0c7c6b295d589637d9ba1c4d0e80b2fdf
Mauricio Leal <mauricio@redbook.local>
2020-07-02 19:09:00 -0300

    Removed commented code

--- src/app/utils/utils.ts
+++ src/app/utils/utils.ts
@@ -1,12 +1,3 @@
-// export function accessibleRouteChangeHandler2() {
-//   return window.setTimeout(() => {
-//     const mainContainer = document.getElementById('primary-app-container');
-//     if (mainContainer) {
-//       mainContainer.focus();
-//     }
-//   }, 50);
-// }
-
 export const accessibleRouteChangeHandler = (): number => 
   window.setTimeout(() => {
     const mainContainer: HTMLElement | null = document.getElementById('primary-app-container');
