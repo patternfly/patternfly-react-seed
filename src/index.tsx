015ddf4d65356d8f6d89f20b9d372c1afac022f8
Rodion Golovushkin <hard.rese7@gmail.com>
2021-01-22 03:37:58 +0700

    Improve hmr experience for react components (#104)
    * improve hmr experience for react components
    
    * fix hmr for async module

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { App } from '@app/index';
+import App from '@app/index';
 
 if (process.env.NODE_ENV !== "production") {
   const config = {
