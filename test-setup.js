177e422111fa1066a0c5952b2e00666e3acbc175
Eric Gustavsson <egustavs@redhat.com>
2021-05-05 15:51:35 +0200

    chore: upgrade webpack dependencies
    Webpack infra used two dependencies that caused concern.
    First is that the optimize-css-assets-webpack-plugin was
    deprecated in favor of css-minimizer-webpack-plugin.
    
    The second thing was html-webpack-plugin giving a deprecation warning
    for one of its dependencies, latest version fixes that. See:
    https://github.com/jantimon/html-webpack-plugin/issues/1523

--- test-setup.js
+++ test-setup.js
@@ -1,5 +1,4 @@
 import { configure } from 'enzyme';
-// import ReactSixteenAdapter from 'enzyme-adapter-react-16';
 import ReactSeventeenAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 configure({ adapter: new ReactSeventeenAdapter() });
