a6b8c79f061808864ce70a2aa1cf07458fa28212
Mike Turley <mike.turley@alum.cs.umass.edu>
2022-04-28 17:27:20 -0400

    Upgrade webpack-dev-server to v4 and other webpack dependencies to latest

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,32 +1,34 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
 const path = require('path');
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common.js");
-const { stylePaths } = require("./stylePaths");
-const HOST = process.env.HOST || "localhost";
-const PORT = process.env.PORT || "9000";
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const { stylePaths } = require('./stylePaths');
+const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || '9000';
 
 module.exports = merge(common('development'), {
-  mode: "development",
-  devtool: "eval-source-map",
+  mode: 'development',
+  devtool: 'eval-source-map',
   devServer: {
-    contentBase: "./dist",
     host: HOST,
     port: PORT,
-    compress: true,
-    inline: true,
     historyApiFallback: true,
-    overlay: true,
-    open: true
+    open: true,
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    client: {
+      overlay: true,
+    },
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        include: [
-          ...stylePaths
-        ],
-        use: ["style-loader", "css-loader"]
-      }
-    ]
-  }
+        include: [...stylePaths],
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 });
