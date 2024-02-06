ab012790c30a3b60d3e3d8cda56731fe88d07b09
Evan <evan.wilkinson@redhat.com>
2024-02-05 22:52:22 -0500

    add react-table dep, update empty state

--- src/app/Inventory/Inventory.tsx
+++ src/app/Inventory/Inventory.tsx
@@ -13,7 +13,6 @@ import {
   Popper,
   Pagination,
   EmptyState,
-  EmptyStateHeader,
   EmptyStateFooter,
   EmptyStateBody,
   Button,
@@ -23,7 +22,6 @@ import {
   ToolbarFilter,
   ToolbarToggleGroup,
   EmptyStateActions,
-  EmptyStateIcon,
 } from '@patternfly/react-core';
 import { Table, Thead, Tr, Th, Tbody, Td } from '@patternfly/react-table';
 import SearchIcon from '@patternfly/react-icons/dist/esm/icons/search-icon';
@@ -625,8 +623,7 @@ export const FilterAttributeSearch: React.FunctionComponent = () => {
   );
 
   const emptyState = (
-    <EmptyState>
-      <EmptyStateHeader headingLevel="h4" titleText="No results found" icon={<EmptyStateIcon icon={SearchIcon} />} />
+    <EmptyState headingLevel="h4" titleText="No results found" icon={SearchIcon}>
       <EmptyStateBody>No results match the filter criteria. Clear all filters and try again.</EmptyStateBody>
       <EmptyStateFooter>
         <EmptyStateActions>
